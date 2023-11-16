4d488792ab72cb518e4ed983ad84febb3600862d
malachy <malachyomeje@gmail.com>
2023-11-16 17:05:58 +0100

    security commit

---
+++ src/main/java/com/payswift/auth/AuthenticationController.java
@@ -0,0 +1,28 @@
+package com.payswift.auth;
+
+import com.payswift.dtos.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v0/auth")
+public class AuthenticationController {
+
+
+
+    private final AuthenticationService service;
+
+    @PostMapping("/authenticate")
+    public BaseResponse register(@RequestBody AuthenticationRequest request)
+
+    {
+        return service.authenticate(request);
+    }
+}
+
+
