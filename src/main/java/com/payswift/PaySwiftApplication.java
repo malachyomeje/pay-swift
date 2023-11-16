4df1aeb979583f9e51d2c5a1b952676d39b6fc63
malachy <malachyomeje@gmail.com>
2023-11-16 15:55:31 +0100

    first commit

---
+++ src/main/java/com/payswift/PaySwiftApplication.java
@@ -0,0 +1,13 @@
+package com.payswift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaySwiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaySwiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payswift/PayswiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payswift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayswiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
