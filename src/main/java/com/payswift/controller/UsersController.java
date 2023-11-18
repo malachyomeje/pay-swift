a392710c2f338fb51d0863b8c20d15aac51a288a
malachy <malachyomeje@gmail.com>
2023-11-18 17:24:28 +0100

    confirmRegistration commit

--- src/main/java/com/payswift/controller/UsersController.java
+++ src/main/java/com/payswift/controller/UsersController.java
@@ -4,14 +4,11 @@ import com.payswift.dtos.request.UsersDto;
 import com.payswift.dtos.response.BaseResponse;
 import com.payswift.service.UsersService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("appUser")
+@RequestMapping("api/v1/appUser")
 public class UsersController {
 
     private final UsersService usersService;
@@ -19,6 +16,14 @@ public class UsersController {
     @PostMapping("signUpUser")
     public BaseResponse signUpUser(@RequestBody UsersDto usersDto){
         return usersService.signUpUser(usersDto);
+
+    }
+
+
+    @PostMapping("confirmRegistration/{token}")
+    public BaseResponse confirmRegistration(@PathVariable String token){
+        return usersService.confirmRegistration(token);
+
 
     }
 }
--- src/main/java/com/payswift/service/serviceImp/UsersServiceImp.java
+++ src/main/java/com/payswift/service/serviceImp/UsersServiceImp.java
@@ -1,14 +1,17 @@
 package com.payswift.service.serviceImp;
 
 import com.payswift.config.JwtService;
+import com.payswift.dtos.request.EmailDto;
 import com.payswift.dtos.request.UsersDto;
 import com.payswift.dtos.response.BaseResponse;
 import com.payswift.enums.Sex;
 import com.payswift.enums.UserRole;
 import com.payswift.model.Users;
 import com.payswift.repository.UsersRepository;
+import com.payswift.service.EmailService;
 import com.payswift.service.UsersService;
 import com.payswift.utils.UsersUtils;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -22,6 +25,8 @@ public class UsersServiceImp implements UsersService {
     private  final UsersRepository usersRepository;
     private  final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private  final EmailService emailService;
+    private final HttpServletRequest servletRequest;
 
 
     @Override
@@ -54,8 +59,42 @@ public class UsersServiceImp implements UsersService {
                 .build();
         usersRepository.save(appUsers);
 
+        String http = servletRequest.getProtocol().substring(0,5).toLowerCase();
+        if (!http.contains("s")) {
+            http = http.substring(0,4);
+        }
+
+        String URL = http + "://"+servletRequest.getServerName()+":"+servletRequest.getServerPort()+"api/v1/appUser/confirmRegistration/{token}" + token;
+
+        String link = "<h3>Hello "  + appUsers.getFirstName()  +
+                "<br> Click the link below to activate your account <a href=" + URL + "><br>Activate</a></h3>";
+
+        String subject = "Pay-Pilot Verification Code";
+
+
+        EmailDto emailSenderDto = new EmailDto();
+        emailSenderDto.setTo(appUsers.getEmail());
+        emailSenderDto.setSubject(subject);
+        emailSenderDto.setContent(link);
+        emailService.sendEmail(emailSenderDto);
         return new BaseResponse<>("REGISTRATION SUCCESSFUL",usersDto.getFirstName());
     }
 
 
-}
+@Override
+    public BaseResponse confirmRegistration(String token) {
+                Optional<Users> existingUser = usersRepository.findByConfirmationToken(token);
+        if (existingUser.isEmpty()) {
+            return new BaseResponse("User not found");
+        }
+            Users user = existingUser.get();
+            user.setConfirmationToken(null);
+            user.setIsEmailVerified(true);
+            usersRepository.save(user);
+
+            return  new BaseResponse("Account verification successful)");
+
+        }
+    }
+
+
